5a259e27854a556902d2e033fb99c4e2591f4473
cubovich <>
2021-07-07 14:02:48 +0200

    Add AuthActivity and AuthPagerAdapter for Sign In and Sign Up fragments

---
+++ app/src/main/java/com/example/coretech_mobile/auth/AuthPagerAdapter.java
@@ -0,0 +1,32 @@
+package com.example.coretech_mobile.auth;
+
+import android.content.Context;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class AuthPagerAdapter extends FragmentPagerAdapter {
+
+    private Context context;
+    private int totalTabs;
+
+    public AuthPagerAdapter(FragmentManager fm, Context context, int totalTabs) {
+        super(fm);
+        this.context = context;
+        this.totalTabs = totalTabs;
+    }
+
+    @Override
+    public int getCount() {
+        return totalTabs;
+    }
+
+    public Fragment getItem(int position) {
+        switch (position) {
+            default:
+                return null;
+        }
+    }
+}
+
